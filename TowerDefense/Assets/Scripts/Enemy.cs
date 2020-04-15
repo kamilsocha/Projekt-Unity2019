772e1646cc4b36e4733b77ab18e044d8e66844f4
kamilsocha <kamil.socha4@gmail.com>
2020-04-16 01:20:11 +0200

    Added BasicEnemy pathfinding

---
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float startSpeed = 10f;
+    [HideInInspector]
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = startSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TowerDefense/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyMovement : MonoBehaviour
+{
+    Transform target;
+    int wavepointIndex = 0;
+    float radius = 0.4f;
+
+    Enemy enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+        target = Waypoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir * enemy.speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= radius)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if(wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            EndPath();
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+
+    void EndPath()
+    {
+        Destroy(gameObject);
+    }
+}
