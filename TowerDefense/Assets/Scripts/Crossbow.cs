caf610cf3b2944024943fd7112f915452a3244b7
kamilsocha <kamil.socha4@gmail.com>
2020-04-18 23:41:46 +0200

    Added enemy tracking script

---
+++ TowerDefense/Assets/Scripts/Crossbow.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crossbow : MonoBehaviour
+{
+    Transform target;
+
+    [Header("General")]
+    public float range = 15f;
+
+    //TODO public GameObject bullet? Prefab
+    public float fireRate = 1f;
+    float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
+    public string enemyTag = "Enemy";
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
+
+    //TODO public Transform firePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+        LockOnTarget();
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach  (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+            //TODO
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void LockOnTarget()
+    {
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
