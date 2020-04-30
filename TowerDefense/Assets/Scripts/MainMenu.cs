10d7913f111ecef2afb8b07094bc36ca675fe1dd
kamilsocha <kamil.socha4@gmail.com>
2020-04-30 16:10:10 +0200

    MainMenu layout reorganization

--- TowerDefense/Assets/Scripts/MainMenu.cs
+++ TowerDefense/Assets/Scripts/MainMenu.cs
@@ -3,14 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public SceneFader sceneFader;
+
     public void Play(string levelToLoad)
     {
         if (levelToLoad == null) levelToLoad = "SelectLevel";
-        SceneManager.LoadScene(levelToLoad);
+        sceneFader.FadeTo(levelToLoad);
     }
+
     public void Quit()
     {
         Debug.Log("Quiting the game...");
         Application.Quit();
+    }
+
+    public void Options()
+    {
+        //TODO
+        Debug.Log("Loading options menu.");
+    }
+
+    public void WallOfGlory()
+    {
+        //TODO
+        Debug.Log("Loading highscores scene.");
+    }
+
+    public void Credits()
+    {
+        //TODO
+        Debug.Log("Loading credits scene.");
     }
 }
