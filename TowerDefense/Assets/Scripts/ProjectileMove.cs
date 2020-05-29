a1b979ee7dff43645f2dc68b0742ad4c89ccda81
kamilsocha <kamil.socha4@gmail.com>
2020-05-30 01:37:15 +0200

    More shaders added.

---
+++ TowerDefense/Assets/Scripts/ProjectileMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    public float speed;
+    public GameObject impactPrefab;
+    public List<GameObject> trails;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(speed != 0 && rb != null)
+        {
+            rb.position += transform.forward * speed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
+
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+
+        if(impactPrefab != null)
+        {
+            var impactVFX = Instantiate(impactPrefab, pos, rot);
+            Destroy(impactVFX, 3);
+        }
+
+        if(trails.Count > 0)
+        {
+            for(int i = 0; i < trails.Count; i++)
+            {
+                trails[i].transform.parent = null;
+                var particleSystem = trails[i].GetComponent<ParticleSystem>();
+                if(particleSystem != null)
+                {
+                    particleSystem.Stop();
+                    Destroy(particleSystem.gameObject, particleSystem.main.duration + particleSystem.main.startLifetime.constantMax);
+                }
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ TowerDefense/Assets/Scripts/SpawnMeteor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SpawnMeteor : MonoBehaviour
+{
+    public GameObject vfx;
+    public Transform startPoint;
+    public Transform endPoint;
+
+
+    void Start()
+    {
+        var startPos = startPoint.position;
+        GameObject objVFX = Instantiate(vfx, startPos, Quaternion.identity);
+
+        var endPos = endPoint.position;
+
+        RotateTo(objVFX, endPos);
+    }
+
+    void RotateTo(GameObject obj, Vector3 destination)
+    {
+        var direction = destination - obj.transform.position;
+        var rotation = Quaternion.LookRotation(direction);
+        obj.transform.localRotation = Quaternion.Lerp(obj.transform.rotation, rotation, 1);
+    }
+
+}
